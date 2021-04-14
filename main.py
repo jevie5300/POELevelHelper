706f0d523e214e30b9c20ac7eac619ca8488d3dc
John <jeblight@gmail.com>
2021-04-14 16:53:52 +0200

    Added Gem level notifications

--- main.py
+++ main.py
@@ -6,6 +6,22 @@ path = "C:/Program Files (x86)/Steam/steamapps/common/Path of Exile/logs/Client.
 print("Do you want to start a new run?y/n")
 new_run = input()
 
+gems = []
+
+# Load Run Gems
+with open("gems.txt", "r", encoding="latin1")as f:
+    for l in f:
+        line = l.split(",")
+        gem_level = line[0]
+        gem_name = line[1]
+        gem_location = line[2]
+        gem_link = line[3]
+
+        gem = [gem_level, gem_name, gem_location, gem_link]
+
+        gems.append(gem)
+
+# Ask user if they want to start a new run
 if new_run == 'y':
     route_file = open("progress.default", encoding="latin1")
     route = route_file.readlines()
@@ -34,7 +50,15 @@ try:
                         actions = route[0].split("-")
                         for a in actions:
                             print(a.strip("\n"))
-        with open("current_run.txt", "w") as f:
+            if "is now level" in current_line:
+                level = current_line.index("is now level ") + 13
+                current_level = current_line[level:]
+                for g in gems:
+                    if g[0] == current_level:
+                        print(g[1] + " available from: " + g[2] + " || For " + g[3])
+                        g[0] = -1
+
+        with open("current_run.txt", "w", encoding="latin1") as f:
             f.writelines(route)
         time.sleep(1)
 except KeyboardInterrupt:
